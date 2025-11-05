818f8cd6cc96773cde48a942c5e944e9a877103c
Vikash Kushwaha <alfredgerogiannis8@gmail.com>
2025-11-05 05:47:10 -0500

    Delete render directory

--- render/render.config.js
+++
@@ -1,7 +0,0 @@
-// Render.com configuration file
-module.exports = {
-  // This is a simple config file to help Render.com detect the project type
-  // The actual configuration is in render.yaml
-  version: '1.0.0',
-  description: 'Render.com Clone Application'
-}
